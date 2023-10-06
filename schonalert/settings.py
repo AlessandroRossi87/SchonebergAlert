b21232e0ff6b45863e3ce96c1c1831f285a91540
AlessandroRossi87 <>
2023-10-06 14:44:41 +0000

    Added local host to settings.py

--- schonalert/settings.py
+++ schonalert/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['schoneberg-alert.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['schoneberg-alert.herokuapp.com', 'schoneberg-alert-aa8dfa8e8adb.herokuapp.com']
 
 
 # Application definition
