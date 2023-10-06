cebea59e041220fa5a6c670c3ec8324af2ed83bd
AlessandroRossi87 <>
2023-10-06 14:58:22 +0000

    Added new allowed host

--- schonalert/settings.py
+++ schonalert/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['schoneberg-alert.herokuapp.com', 'localhost',
-                 '8000-alessandrorossi87-schone-o29nuro7ry.us2.codeanyapp.com']
+                 '8000-alessandrorossi87-schone-o29nuro7ry.us2.codeanyapp.com', 'schoneberg-alert-aa8dfa8e8adb.herokuapp.com']
 
 
 # Application definition
