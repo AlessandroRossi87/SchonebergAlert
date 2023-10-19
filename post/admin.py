460691119fa8d37a7f123be5da9e8cc61d9d2fe2
AlessandroRossi87 <>
2023-10-19 11:44:25 +0000

    Worked on comment section in template

--- post/admin.py
+++ post/admin.py
@@ -10,4 +10,10 @@ admin.site.register(Post)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('created_by', 'text', 'post', 'created_on', 'active')
     list_filter = ('active', 'created_on')
-    search_fields = ('created_by', 'text')
+    search_fields = ('created_by__username', 'text')
+    actions = ['publish_comments']
+
+    def publish_comments(self, request, queryset):
+        queryset.update(active=True)
+
+    publish_comments.short_description = "Publish selected comments"
